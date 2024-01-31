6faee2039e5479afcccb953d260fb2a45a0c4c90
Mit Mathukiya <54717452+Mitpatel13@users.noreply.github.com>
2024-01-31 14:37:28 +0530

    change in login response

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,34 +11,34 @@ module.exports = {
         });
         try{
             const savedUser  = await newUser.save();
-            res.status(200).send({"msg":"Registration Success",savedUser})
+            res.status(200).send({s:0,m:"Registration Success",r:savedUser})
         }
         catch(e){
-            res.status(500).send({'msg':e.message,});
+            res.status(500).send({m:e.message,s:1});
         }
     },
     loginUser :async(req,res)=>{
         try {
             const user = await User.findOne({ email: req.body.email });
             if (!user) {
-                return res.status(401).json({ msg: "Invalid Credentials" });
+                return res.status(401).json({ m: "Invalid Credentials",s:1 });
             }
             
             const decryptPass = CryptoJs.AES.decrypt(user.password, process.env.SECRET);
             const depassword = decryptPass.toString(CryptoJs.enc.Utf8);
     
             if (depassword !== req.body.password) {
-                return res.status(401).json({ msg: "Invalid password" });
+                return res.status(401).json({ m: "Invalid password" ,s:1});
             }
             const {password,__v,createdAt,...others} = user._doc;
             const userToken = jwt.sign({
                 id:user._id,isAdmin:user.isAdmin,isAgent:user.isAgent
             },process.env.JWT_SEC,{expiresIn:"21d"});
     
-            res.status(200).json({ msg: "Login successful", ...others,userToken });
+            res.status(200).json({ s:0,m: "Login successful", r:{...others,userToken} });
         } catch (e) {
             console.error(e);
-            res.status(500).json({ msg:e.message });
+            res.status(500).json({ m:e.message,s:1 });
         }
         }
 }
