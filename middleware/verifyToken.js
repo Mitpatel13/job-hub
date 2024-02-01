9629fa2fa24a53ab4b3e4af2c030a44d50b47b02
Mit Mathukiya <54717452+Mitpatel13@users.noreply.github.com>
2024-02-01 19:22:26 +0530

    change in token

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,24 +1,24 @@
 const User = require('../models/User');
 const jwt = require("jsonwebtoken");
-const verifyToken = async(req,res,next)=>{
-    try{
-        var authHeader= req.headers['token'];
-        if(authHeader){
-            const token = authHeader.split(" ")[1];
-            jwt.verify(token,process.env.JWT_SEC,async(error,user)=>{
-                if(error) res.status(403).send({"msg":"Invalid token"});
-                req.user = user;
-                next();
-            })
-        }
-        else{
-            return res.status(401).send({"msg":"You are not authenticated"})
-        }
+const verifyToken = async (req, res, next) => {
+    const token = req.headers['token'];
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. Token is missing.' });
     }
-    catch(e){
-        res.status(500).send({"msg":e.message})
+
+    try {
+        const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SEC);
+        const user = await User.findById(decoded.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+        req.user = user; // Set req.user with the user information
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(403).json({ message: 'Invalid token.' });
     }
-}
+};
 const verifyAndAuthorization = async (req,res,next)=>{
     try{
         verifyToken(req,res,()=>{if(req.user.id === req.params.id){
